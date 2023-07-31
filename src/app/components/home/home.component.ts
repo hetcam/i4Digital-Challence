c44790095f140019c4ebec5399fff933b4bef1ee
Camilo Bermudez <hbermudez@technisys.com>
2023-07-31 00:03:17 -0500

    Fixed home components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,13 +20,4 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  /*meals: Meal[] = [
-    new Meal('A Test Recipe', 'This is a simply test', 'https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg'),
-    new Meal('A Test Recipe', 'This is a simply test', 'https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg'),
-    new Meal('A Test Recipe', 'This is a simply test', 'https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg'),
-    new Meal('A Test Recipe', 'This is a simply test', 'https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg'),
-    new Meal('A Test Recipe', 'This is a simply test', 'https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg'),
-    new Meal('A Test Recipe', 'This is a simply test', 'https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg')
-  ];*/
-
 }
